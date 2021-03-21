8006cd676e51f1eed14c1973e07035504afe30a4
pgs8 <pgs8@njit.edu>
2021-03-21 15:42:23 -0400

    Feature | Addition unit test

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,12 @@
+from Addition import addition
+
 
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,5 +1,24 @@
 import unittest
+import csv
 from Calculator import Calculator
+
+test_data_file_object = None
+test_data_row_list = list()
+
+
+def load_test_data(test_data_file_path):
+    global test_data_file_object, test_data_row_list
+    test_data_file_object = open(test_data_file_path, 'r')
+    csv_reader = csv.reader(test_data_file_object, delimiter=',')
+    for row in csv_reader:
+        test_data_row_list.append(row)
+
+
+def close_test_data_file():
+    global test_data_file_object
+    if test_data_file_object is not None:
+        test_data_file_object.close()
+        test_data_file_object = None
 
 
 class MyTestCase(unittest.TestCase):
@@ -7,8 +26,22 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.calculator = Calculator()
 
+    def tearDown(self) -> None:
+        if self.calculator is not None:
+            self.calculator = None
+        close_test_data_file()
+
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
+
+    def test_add_method_calculator(self):
+        load_test_data('../tests/Unit Test Addition.csv')
+        for row in test_data_row_list:
+            x = row[0]
+            y = row[1]
+            expect_result = row[2]
+            result = self.calculator.add(x, y)
+            self.assertEqual(float(result), float(expect_result))
 
 
 if __name__ == '__main__':
